79043bfa831c3c148a52b657f553de265d428197
Moheeb Aljaroudi <moheeb@ksu.edu>
2022-05-03 00:49:24 -0500

    making clippy happy

--- src/funcs/file_content.rs
+++ src/funcs/file_content.rs
@@ -2,12 +2,12 @@ use super::Runner;
 use serde::Deserialize;
 use std::fs::File;
 use std::fs::OpenOptions;
+use std::io::Read;
 use std::io::Write;
 use std::io::{BufRead, BufReader, BufWriter};
+use std::io::{Seek, SeekFrom};
 use std::path::Path;
 use std::path::PathBuf;
-use std::io::Read;
-use std::io::{Seek,SeekFrom};
 #[derive(Deserialize)]
 pub struct LineInFile {
     name: String,
@@ -54,68 +54,59 @@ struct BlockInFile {
     comment: String,
 }
 
-
 #[typetag::deserialize]
 impl Runner for BlockInFile {
-fn run(&mut self) -> Result<(), std::io::Error> {
-
-    println!("=================================================");
-    println!("TASK {}", self.name);
-    let spattern = format!("{} ~!STARTING TRUN {}", &self.comment, &self.signature);
-    let epattern = format!("{} !~ENDING TRUN {}", &self.comment, &self.signature);
-    let source_file = Path::new(&self.file);
-    if !source_file.exists() {
+    fn run(&mut self) -> Result<(), std::io::Error> {
+        println!("=================================================");
+        println!("TASK {}", self.name);
+        let spattern = format!("{} ~!STARTING TRUN {}", &self.comment, &self.signature);
+        let epattern = format!("{} !~ENDING TRUN {}", &self.comment, &self.signature);
+        let source_file = Path::new(&self.file);
+        if !source_file.exists() {
             let mut created = File::create(&self.file)?;
-            writeln!(created,"{}", &spattern)?;
-            writeln!(created,"")?;
-            writeln!(created,"{}", &self.block)?;
-            writeln!(created,"")?;
-            writeln!(created,"{}", &epattern)?;
+            writeln!(created, "{}", &spattern)?;
+            writeln!(created)?;
+            writeln!(created, "{}", &self.block)?;
+            writeln!(created)?;
+            writeln!(created, "{}", &epattern)?;
             println!("=================================================");
             return Ok(());
         }
 
-        let mut opened = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .open(&self.file)?;
+        let mut opened = OpenOptions::new().read(true).write(true).open(&self.file)?;
         let mut buffer = BufReader::new(&opened);
         let mut stringed_buffer = String::new();
-         buffer.read_to_string(&mut stringed_buffer)?;
-                let s_index = stringed_buffer.find(&spattern);
-        if let Some(start) = s_index  {
+        buffer.read_to_string(&mut stringed_buffer)?;
+        let s_index = stringed_buffer.find(&spattern);
+        if let Some(start) = s_index {
             let e_index = stringed_buffer.find(&epattern);
-            if let Some(end) =  e_index {
-                if &self.block == &stringed_buffer[start+spattern.len()..end].trim() {
+            if let Some(end) = e_index {
+                if self.block == stringed_buffer[start + spattern.len()..end].trim() {
                     println!("BLOCK FOUND");
                     println!("=================================================");
                     return Ok(());
                 }
                 println!("BLOCK DOESN'T MATCH");
-                
+
                 opened.seek(SeekFrom::Start(start as u64))?;
-                opened.write_all(&b" ".repeat(end+epattern.len()-(start )))?;
+                opened.write_all(&b" ".repeat(end + epattern.len() - (start)))?;
                 opened.seek(SeekFrom::Start((start) as u64))?;
-                writeln!(opened,"{}", &spattern)?;
-                writeln!(opened,"")?;
-                writeln!(opened,"{}", &self.block)?;
-                writeln!(opened,"")?;
-                writeln!(opened,"{}", &epattern)?;
+                writeln!(opened, "{}", &spattern)?;
+                writeln!(opened)?;
+                writeln!(opened, "{}", &self.block)?;
+                writeln!(opened)?;
+                writeln!(opened, "{}", &epattern)?;
                 //opened.write(&self.block.as_bytes())?;
                 println!("FINISHED WRITING");
                 println!("=================================================");
-         return Ok(());
-                // Block Doesn't Match, change it 
+                return Ok(());
+                // Block Doesn't Match, change it
             }
-            
         }
-       // let mut buf_writer = BufWriter::new(opened);
-       // buf_writer.write_all(self.block.as_bytes())?;
-       // println!("FINISHED WRITING");
-       // println!("=================================================");
+        // let mut buf_writer = BufWriter::new(opened);
+        // buf_writer.write_all(self.block.as_bytes())?;
+        // println!("FINISHED WRITING");
+        // println!("=================================================");
         Ok(())
-
-
-}
-
+    }
 }
